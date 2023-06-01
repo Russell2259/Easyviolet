7a79ca1348089e69da5f288d626c6657b72f554e
Russell2259 <russell2259@tuta.io>
2023-05-31 20:08:37 -0600

    use static bundle

--- examples/http.mjs
+++ examples/http.mjs
@@ -3,9 +3,7 @@ import http from 'node:http';
 
 const server = http.createServer();
 
-const ultraviolet = new Easyviolet();
-
-ultraviolet.httpServer(server);
+new Easyviolet().httpServer(server);
 
 server.listen(8080, () => {
     console.log(`Your easyviolet demo is running on port ${server.address().port}`);
