384fbc169d64e7c26f47fca998b1172099995620
Russell2259 <russell2259@tuta.io>
2023-06-28 19:15:04 -0600

    update bare server node

--- examples/express.mjs
+++ examples/express.mjs
@@ -1,10 +1,10 @@
-import Easyviolet from 'easyviolet';
+import Easyviolet from '../index.js';
 import express from 'express';
 
 const app = express();
 
-app.use(new Easyviolet().express(app));
-
 const server = app.listen(8080, () => {
   console.log(`Your easyviolet demo is running on port ${server.address().port}`);
 });
+
+new Easyviolet().httpServer(server);
